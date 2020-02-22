4b5f52ac5b2934291d1cbefde8b08eb4c50db83f
DiegoBtts <die-leo@outlook.com>
2020-02-22 11:19:33 -0700

    Creacion del Modelo GameImage con sus respectivas vistas donde ya se pueden cargar y visualizar imagenes extrayendo los datos desde la base de datos con las imagenes serializadas.

---
+++ Memoria/Controllers/GameImagesController.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using Memoria.Data;
+using Memoria.Models;
+
+namespace Memoria.Controllers
+{
+    public class GameImagesController : Controller
+    {
+        private MemoriaContext db = new MemoriaContext();
+
+        // GET: GameImages
+        public ActionResult Index()
+        {
+            return View(db.GameImages.ToList());
+        }
+
+        // GET: GameImages/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GameImage gameImage = db.GameImages.Find(id);
+            if (gameImage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gameImage);
+        }
+
+        // GET: GameImages/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: GameImages/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdImage,Name,Portada,Image0,Image1,Image2,Image3,Image4,Image5,Image6,Image7,Image8,Image9")] GameImage gameImage)
+        {
+            for (int i = 0; i < Request.Files.Count; i++)
+            {
+                HttpPostedFileBase FileBase = Request.Files[i];
+                WebImage image = new WebImage(FileBase.InputStream);
+                switch (i)
+                {
+                    case 0:
+                        gameImage.Portada = image.GetBytes();
+                        break;
+                    case 1:
+                        gameImage.Image0 = image.GetBytes();
+                        break;
+                    case 2:
+                        gameImage.Image1 = image.GetBytes();
+                        break;
+                    case 3:
+                        gameImage.Image2 = image.GetBytes();
+                        break;
+                    case 4:
+                        gameImage.Image3 = image.GetBytes();
+                        break;
+                    case 5:
+                        gameImage.Image4 = image.GetBytes();
+                        break;
+                    case 6:
+                        gameImage.Image5 = image.GetBytes();
+                        break;
+                    case 7:
+                        gameImage.Image6 = image.GetBytes();
+                        break;
+                    case 8:
+                        gameImage.Image7 = image.GetBytes();
+                        break;
+                    case 9:
+                        gameImage.Image8 = image.GetBytes();
+                        break;
+                    case 10:
+                        gameImage.Image9 = image.GetBytes();
+                        break;
+
+                }
+
+            }
+
+
+
+            if (ModelState.IsValid)
+            {
+                db.GameImages.Add(gameImage);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(gameImage);
+        }
+
+        // GET: GameImages/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GameImage gameImage = db.GameImages.Find(id);
+            if (gameImage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gameImage);
+        }
+
+        // POST: GameImages/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdImage,Name,Portada,Image0,Image1,Image2,Image3,Image4,Image5,Image6,Image7,Image8,Image9")] GameImage gameImage)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(gameImage).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(gameImage);
+        }
+
+        // GET: GameImages/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GameImage gameImage = db.GameImages.Find(id);
+            if (gameImage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gameImage);
+        }
+
+        // POST: GameImages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            GameImage gameImage = db.GameImages.Find(id);
+            db.GameImages.Remove(gameImage);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        public ActionResult getImagek(int id, string type)
+        {
+            GameImage imagenek = db.GameImages.Find(id);
+            byte[] byteImage = null;
+            switch (type)
+            {
+                case "Portada":
+                    byteImage = imagenek.Portada;
+                    break;
+                case "Image0":
+                    byteImage = imagenek.Image0;
+                    break;
+                case "Image1":
+                    byteImage = imagenek.Image1;
+                    break;
+                case "Image2":
+                    byteImage = imagenek.Image2;
+                    break;
+                case "Image3":
+                    byteImage = imagenek.Image3;
+                    break;
+                case "Image4":
+                    byteImage = imagenek.Image4;
+                    break;
+                case "Image5":
+                    byteImage = imagenek.Image5;
+                    break;
+                case "Image6":
+                    byteImage = imagenek.Image6;
+                    break;
+                case "Image7":
+                    byteImage = imagenek.Image7;
+                    break;
+                case "Image8":
+                    byteImage = imagenek.Image8;
+                    break;
+                case "Image9":
+                    byteImage = imagenek.Image9;
+                    break;
+
+            }
+            MemoryStream memoryStream = new MemoryStream(byteImage);
+            Image image = Image.FromStream(memoryStream);
+            memoryStream = new MemoryStream();
+            image.Save(memoryStream, ImageFormat.Jpeg);
+            memoryStream.Position = 0;
+            return File(memoryStream, "image/jpg");
+        }
+    }
+}
---
+++ Memoria/Models/GameImage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Memoria.Models
+{
+    public class GameImage
+    {
+        [Key]
+        public int IdImage { get; set; }
+        [DisplayName("Nombre Del Juego")]
+        public string Name { get; set; }
+        public byte[] Portada { get; set; }
+        [DisplayName("Imagen 1")]
+        public byte[] Image0 { get; set; }
+        [DisplayName("Imagen 2")]
+        public byte[] Image1 { get; set; }
+        [DisplayName("Imagen 3")]
+        public byte[] Image2 { get; set; }
+        [DisplayName("Imagen 4")]
+        public byte[] Image3 { get; set; }
+        [DisplayName("Imagen 5")]
+        public byte[] Image4 { get; set; }
+        [DisplayName("Imagen 6")]
+        public byte[] Image5 { get; set; }
+        [DisplayName("Imagen 7")]
+        public byte[] Image6 { get; set; }
+        [DisplayName("Imagen 8")]
+        public byte[] Image7 { get; set; }
+        [DisplayName("Imagen 9")]
+        public byte[] Image8 { get; set; }
+        [DisplayName("Imagen 10")]
+        public byte[] Image9 { get; set; }
+    }
+}
